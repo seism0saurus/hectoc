a24bfead38ed6ea2edd4f856dd1e8bc0a3a6a14d
seism0saurus <github@seism0saurus.de>
2024-04-07 12:18:31 +0200

    Issue #105 - Real precision issue
    https://github.com/seism0saurus/hectoc/issues/105
    
    Add logs to analyse errors in the challenger

--- webapp/src/main/java/de/seism0saurus/hectoc/webapp/webservice/HectocService.java
+++ webapp/src/main/java/de/seism0saurus/hectoc/webapp/webservice/HectocService.java
@@ -3,6 +3,8 @@ package de.seism0saurus.hectoc.webapp.webservice;
 import de.seism0saurus.hectoc.generator.HectocChallenge;
 import de.seism0saurus.hectoc.generator.HectocGenerator;
 import de.seism0saurus.hectoc.shuntingyardalgorithm.HectocSolution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -11,20 +13,26 @@ import java.math.BigDecimal;
 @RestController
 public class HectocService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HectocService.class);
+
     @GetMapping("/hectocs")
     HectocChallengeDto getHectoc() {
+        String challenge = HectocGenerator
+                .generate()
+                .toString();
+        LOGGER.info("Hectoc requested. Sending {}", challenge);
         return new HectocChallengeDto(
-                HectocGenerator
-                        .generate()
-                        .toString());
+                challenge);
     }
 
     @PostMapping("/hectocs/{hectoc}/solution")
     HectocResultDto checkSolution(@PathVariable final String hectoc, @RequestBody final HectocSolutionDto solutionDto) {
+        LOGGER.info("Solution for {} received, trying to check {}", hectoc, solutionDto.getSolution());
         HectocChallenge challenge = new HectocChallenge(hectoc);
         HectocSolution solution = new HectocSolution(challenge);
         boolean valid = solution.checkSolution(solutionDto.getSolution());
         BigDecimal result = solution.getResult();
+        LOGGER.info("Solution {} for {} has validity {} and result {}", solutionDto.getSolution(), hectoc, valid, result);
         return new HectocResultDto(valid, result.intValue());
     }
 }
