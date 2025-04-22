1b000a39ea20e39dfe9d24e22e4fe38172943f2f
seism0saurus <github@seism0saurus.de>
2025-04-22 14:23:15 +0200

    Add comment

--- bruteforcer/src/main/java/de/seism0saurus/hectoc/bruteforcer/scheduling/Scheduler.java
+++ bruteforcer/src/main/java/de/seism0saurus/hectoc/bruteforcer/scheduling/Scheduler.java
@@ -17,6 +17,28 @@ import java.util.UUID;
 
 import static de.seism0saurus.hectoc.generator.HectocGenerator.UNSOLVABLE_HECTOCS;
 
+/**
+ * The Scheduler class is responsible for initializing and managing the scheduling of jobs
+ * that generate number permutations for specific challenges. This class is a Spring
+ * Service and is only activated when the "scheduler" profile is active.
+ * <p>
+ * The class utilizes the {@link JobScheduler} to enqueue jobs for generating permutations
+ * of numbers provided in {@link HectocChallenge} instances that are identified as unsolvable.
+ * Additionally, it ensures that challenges are persisted in the database using the
+ * {@link Repository} and logs the results of each scheduling attempt.
+ * <p>
+ * Responsibilities include:
+ * - Checking if a given unsolvable challenge is already stored in the database.
+ * - Enqueuing a job for unsolvable challenges not found in the database.
+ * - Persisting new challenges into the database.
+ * - Logging information about job scheduling and existing challenges.
+ * <p>
+ * This service ensures that unsolvable challenges are properly scheduled for job processing,
+ * while avoiding duplicate database entries or job submissions.
+ * <p>
+ * An instance of {@link JobScheduler} is used for handling job queuing, while
+ * repository operations are performed through {@link Repository}.
+ */
 @Service
 @Profile("scheduler")
 public class Scheduler {
